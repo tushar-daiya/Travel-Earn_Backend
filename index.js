9c1520b33a7f745b369155ec5c015bef0d493813
Nikhil <nikhilsorout6@gmail.com>
2025-05-12 16:31:57 +0530

    Configured Nginx

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const https = require('https');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const adminRoutes = require('./router/Admin.route')
-const fs = require('fs')
-
+const adminRoutes = require('./router/Admin.route');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-const options = {
-  key: fs.readFileSync('/etc/ssl/myapp/key.pem'),
-  cert: fs.readFileSync('/etc/ssl/myapp/cert.pem'),
-};
-
-
 // Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 const Mongo_URI = process.env.MONGO_URI;
-console.log(`Mongo_URI: ${Mongo_URI}`);
-console.log(`JWT Secret: ${process.env.JWT_SECRET}`);
-console.log(`Port: ${PORT}`);
 
+// console.log(`Mongo_URI: ${Mongo_URI}`);
+// console.log(`JWT Secret: ${process.env.JWT_SECRET}`);
+// console.log(`Port: ${PORT}`);
 
-
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(Mongo_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -47,6 +36,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the server!');
 });
 
-https.createServer(options, app).listen(443, () => {
-  console.log('HTTPS server running with Cloudflare origin cert');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
